6a789599fef71eddd532ce543693dc7a2cf8fbce
Ashly LaMarr <surlymae@gmail.com>
2019-04-03 10:07:32 -0600

    WIP

--- Services/IPersonnelRepo.cs
+++ Services/IPersonnelRepo.cs
@@ -5,15 +5,15 @@ namespace RESTfulAPI.AspNetCore.NewDb.Services
 {
     public interface IPersonnelRepo
     {
-        IEnumerable<Department> GetDepartments();
-        IEnumerable<Department> GetDepartments(IEnumerable<int> deptIds);
-        Department GetDepartment(int deptId);
-        bool DepartmentExists(int deptId);
-        IEnumerable<Employee> GetEmployeesByType(string empType);
-        IEnumerable<Employee> GetEmployeesByDept(int deptId);
-        Employee GetEmployee(int empId);
-        void AddEmployeeToDepartment(int deptId, Employee emp);
-        void UpdateEmployee(Employee emp);
-        bool Save();
+        IEnumerable<Department> GetDepartments ();
+        IEnumerable<Department> GetDepartments (IEnumerable<int> deptIds);
+        Department GetDepartment (int deptId);
+        bool DepartmentExists (int deptId);
+        IEnumerable<Employee> GetEmployeesByType (string empType);
+        IEnumerable<Employee> GetEmployeesByDept (int deptId);
+        Employee GetEmployee (int empId);
+        void AddEmployeeToDepartment (int deptId, Employee emp);
+        void UpdateEmployee (Employee emp);
+        bool Save ();
     }
 }
--- Services/PersonnelRepo.cs
+++ Services/PersonnelRepo.cs
@@ -11,18 +11,35 @@ namespace RESTfulAPI.AspNetCore.NewDb.Services
 
         public PersonnelRepo(PersonnelContext context) => _db = context;
 
-        public IEnumerable<Department> GetDepartments() => _db.Departments.OrderBy(d => d.Name);
+        public IEnumerable<Department> GetDepartments () => _db.Departments.OrderBy(d => d.Name);
 
-        public IEnumerable<Department> GetDepartments(IEnumerable<int> ids) => _db.Departments.Where(d => ids.Contains(d.DepartmentId)).OrderBy(d => d.Name).ToList();
+        public IEnumerable<Department> GetDepartments (IEnumerable<int> ids) => _db.Departments.Where(d => ids.Contains(d.DepartmentId)).OrderBy(d => d.Name).ToList();
 
-        public Department GetDepartment(int id) => _db.Departments.FirstOrDefault(d => d.DepartmentId == id);
+        public Department GetDepartment (int id) => _db.Departments.FirstOrDefault(d => d.DepartmentId == id);
 
-        public bool DepartmentExists(int id) => _db.Departments.Any(d => d.DepartmentId == id);
+        public bool DepartmentExists (int id) => _db.Departments.Any(d => d.DepartmentId == id);
 
-        public IEnumerable<Employee> GetEmployeesByType(string empType) => _db.Employees.Where(e => e.Type == empType).OrderBy(e => e.LastName).ThenBy(e => e.FirstName).ToList();
+        public IEnumerable<Employee> GetEmployeesByType (string empType) => _db.Employees.Where(e => e.Type == empType).OrderBy(e => e.LastName).ThenBy(e => e.FirstName).ToList();
 
-        public IEnumerable<Employee> GetEmployeesByDept(int id) => _db.Employees.Where(e => e.DepartmentId == id).OrderBy(e => e.LastName).ThenBy(e => e.FirstName).ToList();
+        public IEnumerable<Employee> GetEmployeesByDept (int id) => _db.Employees.Where(e => e.DepartmentId == id).OrderBy(e => e.LastName).ThenBy(e => e.FirstName).ToList();
 
-        public Employee GetEmployee(int empId) => _db.Employees.Where(e => e.Id == empId).FirstOrDefault();
+        public Employee GetEmployee (int empId) => _db.Employees.Where(e => e.Id == empId).FirstOrDefault();
+
+        public void AddEmployeeToDepartment (int id, Employee emp)
+        {
+            var dept = GetDepartment(id);
+            if (dept != null)
+            {
+                dept.Employees.Add(emp);
+            }
+        }
+
+        public void UpdateEmployee (Employee emp)
+        {
+            //do we want to allow this?
+            //rules around who can update an employee, would require auth code
+        }
+
+        public bool Save () => _db.SaveChanges() >= 0;
     }
 }
