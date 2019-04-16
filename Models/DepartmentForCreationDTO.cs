72136a548d50350e79dd2d52cb80793bc326f603
Ashly LaMarr <surlymae@gmail.com>
2019-04-16 12:27:05 -0600

    Add POST for list of employees with department

--- Models/DepartmentForCreationDTO.cs
+++ Models/DepartmentForCreationDTO.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
+
 namespace RESTfulAPI.AspNetCore.NewDb.Models
 {
     public class DepartmentForCreationDTO
     {
         public string Name { get; set; }
+        public ICollection<EmployeeForCreationDTO> Employees { get; set; } 
+        = new List<EmployeeForCreationDTO>();
     }
 }
